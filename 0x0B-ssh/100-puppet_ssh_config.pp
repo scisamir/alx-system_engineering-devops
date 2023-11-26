93ee359914079fa3af1ce5a4e95983060abc39f9
Samir Idris <samzidris11@gmail.com>
2023-11-26 13:00:00 -0800

    puppet ssh config

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,13 +1,13 @@
 # Client configuration file (w/ Puppet)
 
 file_line { 'add private key'
-	ensure => 'present'
-	path   => '/etc/ssh/ssh_config'
+	ensure => 'present',
+	path   => '/etc/ssh/ssh_config',
 	line   => '	IdentityFile ~/.ssh/school'
 }
 
 file_line { 'allow auth with no password'
-        ensure => 'present'
-        path   => '/etc/ssh/ssh_config'
+        ensure => 'present',
+        path   => '/etc/ssh/ssh_config',
         line   => '	PasswordAuthentication no'
 }
