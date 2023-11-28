416fab97808e97547c80d85b9b1933625ae9a5de
Samir Idris <samzidris11@gmail.com>
2023-11-28 13:55:14 -0800

    web server

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,21 +1,21 @@
 # install and configure nginx using puppet
 
-package { 'nginx'
+package { 'nginx':
   ensure => 'installed'
 }
 
-exec { 'install nginx and update packages'
+exec { 'install nginx and update packages':
   command => 'sudo apt-get update; sudo apt-get -y install nginx'
 }
 
-exec { 'Hello World'
+exec { 'Hello World':
   command => 'sudo ufw allow 'Nginx HTTP; echo "Hello World!" > /var/www/html/index.html'
 }
 
-exec { 'perform redirection'
+exec { 'perform redirection':
   command => 'sudo sed -i "/listen 80 default_server/a rewrite ^/redirect_me https://github.com permanent;" /etc/nginx/sites-available/default'
 }
 
-exec { 'restart nginx'
+exec { 'restart nginx':
   command => 'sudo service nginx restart'
 }
