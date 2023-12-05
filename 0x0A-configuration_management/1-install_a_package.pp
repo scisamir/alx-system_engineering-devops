3975522c9dffac3d67afb6e9d031d5a3fe389f4d
Samir Idris <samzidris11@gmail.com>
2023-12-04 23:43:59 -0800

    puppet install flask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,23 @@
 # Install a package
 
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
-  path     => '/usr/local/bin/'
+package { 'python3-pip':
+  ensure => 'installed'
+}
+
+exec { 'upgrade pip':
+  command => 'pip3 install --upgrade pip',
+  path    => '/usr/local/bin/',
+  require => Package['python3-pip']
+}
+
+exec { 'install flask':
+  command => 'pip3 install Flask==2.1.0',
+  path    => '/usr/local/bin/',
+  require => [ Package['python3-pip'], Exec['upgrade pip'] ]
+}
+
+exec { 'update werkzeug':
+  command => 'pip3 install --upgrade werkzeug',
+  path    => '/usr/local/bin/',
+  require => Package['python3-pip']
 }
