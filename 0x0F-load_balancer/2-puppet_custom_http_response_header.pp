995253e536b74ae784ef2a46b00f231d82fa99c9
Samir Idris <samzidris11@gmail.com>
2023-12-04 20:50:18 -0800

    puppet add custom http header

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,16 +1,17 @@
 # install and configure nginx using puppet
 
-package { 'nginx':
-  ensure => 'installed'
-}
-
-exec { 'install nginx and update packages':
-  command  => 'sudo apt-get update; sudo apt-get -y install nginx',
+exec { 'update packages':
+  command  => 'sudo apt-get update',
   provider => shell
 }
 
+package { 'nginx':
+  ensure => 'installed',
+  name   => 'nginx'
+}
+
 file { '/var/www/html/index.html':
-  content  => 'Hello World!'
+  content => 'Hello World!'
 }
 
 file_line { 'setup custom http response header':
@@ -20,6 +21,7 @@ file_line { 'setup custom http response header':
   after  => 'http {'
 }
 
-service { 'nginx':
-  ensure => 'running'
+exec { 'restart nginx':
+  command  => 'sudo service nginx restart',
+  provider => shell
 }
