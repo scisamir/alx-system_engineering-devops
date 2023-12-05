49371366c6091c5b55fab794e1558203286fd827
Samir Idris <samzidris11@gmail.com>
2023-12-04 22:32:28 -0800

    custom http response header with nginx

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,22 +1,25 @@
 # install and configure nginx using puppet
 
 exec { 'update packages':
-  command  => 'sudo apt-get update',
+  command  => 'sudo apt-get -y update',
   provider => shell
 }
 
 package { 'nginx':
-  ensure => 'installed',
-  name   => 'nginx'
+  ensure  => 'installed',
+  name    => 'nginx',
+  require => Exec['update packages']
 }
 
 file { '/var/www/html/index.html':
-  content => 'Hello World!'
+  content => 'Hello World!',
+  require => Package['nginx']
 }
 
 exec { 'setup custom http response header':
   command  => "sed -i '/http {/a \\\n\\tadd_header X-Served-By ${hostname};' /etc/nginx/nginx.conf",
-  provider => shell
+  provider => shell,
+  require => Package['nginx']
 }
 
 exec { 'restart nginx':
