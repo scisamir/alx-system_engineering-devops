6f4553f386b05ff5accc272fcabfa8a12225e624
Samir Idris <samzidris11@gmail.com>
2024-01-21 00:19:14 -0800

    web stack debugging #3

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,9 @@
+# This puppet mainfest uses edits a wordpress configuration file in an attempt
+# 	to fix the `500 internal server error`
+
+$file = '/var/www/html/wp-settings.php'
+
+exec { 'fix_file_ext':
+  command => "sed -i 's/class-wp.locale.phpp/class-wp.locale.php/g' ${file}",
+  path    => ['/bin', '/usr/bin']
+}
