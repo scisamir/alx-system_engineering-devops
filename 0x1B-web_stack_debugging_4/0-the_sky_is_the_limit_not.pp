c667e06ea2d59aa56ee3889d21e3d01504485028
Samir Idris <samzidris11@gmail.com>
2024-01-24 20:35:35 +0000

    web stack debugging #4

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,13 @@
+# Fix inabilty of server to handle many concurrent request
+
+$file = '/etc/default/nginx'
+
+exec { 'increase limit':
+  command => "sed -i 's/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/g' ${file}",
+  path    => ['/bin', '/usr/bin']
+}
+
+exec { 'restart nginx':
+  command => 'service nginx restart',
+  path    => ['/bin', '/usr/bin']
+}
